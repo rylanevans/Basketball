67b93e930b4c29449b17ccf6967f92006e9a6497
Rylan <rylanjevans@gmail.com>
2018-05-05 21:29:43 -0600

    Removed Tab Bar Added Side Menu Making it collection views Trying things out

---
+++ Basketball/Views/ContainerVC.swift
@@ -0,0 +1,42 @@
+//
+//  ContainerVC.swift
+//  Basketball
+//
+//  Created by Rylan Evans on 5/5/18.
+//  Copyright © 2018 Rylan Evans. All rights reserved.
+//
+
+import UIKit
+
+class ContainerVC: UIViewController {
+    
+    @IBOutlet weak var sideMenuConstraint: NSLayoutConstraint!
+    var sideMenuOpen = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(toggleSideMenu),
+                                               name: NSNotification.Name("ToggleSideMenu"),
+                                               object: nil)
+    }
+    
+    @objc func toggleSideMenu() {
+        if sideMenuOpen {
+            sideMenuOpen = false
+            sideMenuConstraint.constant = -240
+            
+        } else {
+            sideMenuOpen = true
+            sideMenuConstraint.constant = 0
+        }
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    
+}
+
+
---
+++ Basketball/Views/PlayersVC.swift
@@ -0,0 +1,51 @@
+//
+//  PlayersVC.swift
+//  Basketball
+//
+//  Created by Rylan Evans on 5/5/18.
+//  Copyright © 2018 Rylan Evans. All rights reserved.
+//
+
+import UIKit
+
+class PlayersVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(showProfile),
+                                               name: NSNotification.Name("ShowProfile"),
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(showSettings),
+                                               name: NSNotification.Name("ShowSettings"),
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(showSignIn),
+                                               name: NSNotification.Name("ShowSignIn"),
+                                               object: nil)
+    }
+    
+    @objc func showProfile() {
+        performSegue(withIdentifier: "ShowProfile", sender: nil)
+    }
+    
+    @objc func showSettings() {
+        performSegue(withIdentifier: "ShowSettings", sender: nil)
+    }
+    
+    @objc func showSignIn() {
+        performSegue(withIdentifier: "ShowSignIn", sender: nil)
+    }
+    
+    
+    @IBAction func onMoreTapped() {
+        print("TOGGLE SIDE MENU")
+        NotificationCenter.default.post(name: NSNotification.Name("ToggleSideMenu"), object: nil)
+    }
+    
+    
+    
+}
+

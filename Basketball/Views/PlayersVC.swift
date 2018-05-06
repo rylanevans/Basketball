26f2f4934084f63f58e5f546ee2d9c5522b9b8ad
Rylan <rylanjevans@gmail.com>
2018-05-06 00:09:23 -0600

    Working with side menu

--- Basketball/Views/PlayersVC.swift
+++ Basketball/Views/PlayersVC.swift
@@ -1,5 +1,5 @@
 //
-//  PlayersVC.swift
+//  PlayersCVC.swift
 //  Basketball
 //
 //  Created by Rylan Evans on 5/5/18.
@@ -8,35 +8,53 @@
 
 import UIKit
 
-class PlayersVC: UIViewController {
+private let reuseIdentifier = "Cell"
+
+class PlayersCVC: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        // Do any additional setup after loading the view.
+        
         NotificationCenter.default.addObserver(self,
-                                               selector: #selector(showProfile),
-                                               name: NSNotification.Name("ShowProfile"),
+                                               selector: #selector(showGames),
+                                               name: NSNotification.Name("ShowGames"),
                                                object: nil)
         NotificationCenter.default.addObserver(self,
-                                               selector: #selector(showSettings),
-                                               name: NSNotification.Name("ShowSettings"),
+                                               selector: #selector(showStats),
+                                               name: NSNotification.Name("ShowStats"),
                                                object: nil)
         NotificationCenter.default.addObserver(self,
-                                               selector: #selector(showSignIn),
-                                               name: NSNotification.Name("ShowSignIn"),
+                                               selector: #selector(showHelp),
+                                               name: NSNotification.Name("ShowHelp"),
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(showDeveloper),
+                                               name: NSNotification.Name("ShowDeveloper"),
                                                object: nil)
     }
     
-    @objc func showProfile() {
-        performSegue(withIdentifier: "ShowProfile", sender: nil)
+    @objc func showGames() {
+        performSegue(withIdentifier: "ShowGames", sender: nil)
     }
     
-    @objc func showSettings() {
-        performSegue(withIdentifier: "ShowSettings", sender: nil)
+    @objc func showStats() {
+        performSegue(withIdentifier: "ShowStats", sender: nil)
     }
     
-    @objc func showSignIn() {
-        performSegue(withIdentifier: "ShowSignIn", sender: nil)
+    @objc func showHelp() {
+        performSegue(withIdentifier: "ShowHelp", sender: nil)
+    }
+    
+    @objc func showDeveloper() {
+        performSegue(withIdentifier: "ShowDeveloper", sender: nil)
     }
     
     
@@ -45,7 +63,68 @@ class PlayersVC: UIViewController {
         NotificationCenter.default.post(name: NSNotification.Name("ToggleSideMenu"), object: nil)
     }
     
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
     
+    // MARK: UICollectionViewDataSource
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        // Configure the cell
+        
+        return cell
+    }
+    
+    // MARK: UICollectionViewDelegate
+    
+    /*
+     // Uncomment this method to specify if the specified item should be highlighted during tracking
+     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment this method to specify if the specified item should be selected
+     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+     
+     }
+     */
     
 }
+
 
--- Basketball/Views/SideMenuVC.swift
+++ Basketball/Views/SideMenuVC.swift
@@ -14,10 +14,32 @@ class SideMenuVC: UITableViewController {
         print(indexPath.row)
         NotificationCenter.default.post(name: NSNotification.Name("ToggleSideMenu"), object: nil)
         
-        switch indexPath.row {
-        case 0: NotificationCenter.default.post(name: NSNotification.Name("ShowProfile"), object: nil)
-        case 1: NotificationCenter.default.post(name: NSNotification.Name("ShowSettings"), object: nil)
-        case 2: NotificationCenter.default.post(name: NSNotification.Name("ShowSignIn"), object: nil)
+        switch indexPath.section {
+        case 0:
+            switch indexPath.row {
+            case 0: self.tableView.reloadData()
+            case 1: NotificationCenter.default.post(name: NSNotification.Name("ShowGames"), object: nil)
+                self.tableView.reloadData()
+            case 2: NotificationCenter.default.post(name: NSNotification.Name("ShowStats"), object: nil)
+                self.tableView.reloadData()
+            default: break
+        }
+        case 1:
+            switch indexPath.row {
+            case 0: self.tableView.reloadData()
+            case 1: self.tableView.reloadData()
+            case 2: self.tableView.reloadData()
+            case 3: self.tableView.reloadData()
+            default: break
+            }
+        case 2:
+            switch indexPath.row {
+            case 0: NotificationCenter.default.post(name: NSNotification.Name("ShowHelp"), object: nil)
+                self.tableView.reloadData()
+            case 1: NotificationCenter.default.post(name: NSNotification.Name("ShowDeveloper"), object: nil)
+                self.tableView.reloadData()
+            default: break
+            }
         default: break
         }
     }
